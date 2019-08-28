09718a9b5277303fa252604f0bc972bb72d42725
Josh <joshdibble93@gmail.com>
2019-08-28 22:50:56 +0100

    Added frontend and fixed almighty backend buggery

---
+++ backend/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyBLBM3vjgkDzjKwJvFJsqx0rEhe7Uj2Iuk',
+  authDomain: 'social-44.firebaseapp.com',
+  databaseURL: 'https://social-44.firebaseio.com',
+  projectId: 'social-44',
+  storageBucket: 'social-44.appspot.com',
+  messagingSenderId: '770034608691',
+  appId: '1:770034608691:web:2eef1403de96fc06'
+};
