2173cf055c5bb074ff1813638ad73fd8b72e4dd7
Josh <joshdibble93@gmail.com>
2019-08-31 20:31:44 +0100

    Added basic UI for login page and some error handling

--- frontend/src/components/Scream.js
+++ frontend/src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import Link from 'react-router-dom/Link';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 // MUI stuff
 import Card from '@material-ui/core/Card';
@@ -24,6 +26,7 @@ const styles = {
 
 export class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -44,17 +47,17 @@ export class Scream extends Component {
           title='Profile Image'
           className={classes.image}
         />
-        <CardContent class={classes.content}>
+        <CardContent className={classes.content}>
           <Typography
             variant='h5'
             component={Link}
             to={`/users/${userHandle}`}
-            color='Primary'
+            color='primary'
           >
             {userHandle}
           </Typography>
           <Typography variant='body2' color='textSecondary'>
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant='body1'>{body}</Typography>
         </CardContent>
