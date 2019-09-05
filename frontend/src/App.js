366da3b630dcadc6fb89c524d8b68294dd2d090f
Josh <joshdibble93@gmail.com>
2019-09-05 20:40:29 +0100

    Added auth routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,36 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import themeFile from './util/theme';
+import jwtDecode from 'jwt-decode';
+
+// MUI stuff
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Components
 import Navbar from './components/Navbar';
+import AuthRoute from './util/AuthRoute';
 
 // Pages
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
-const theme = createMuiTheme({
-  palette: {
-    common: { black: '#000', white: '#fff' },
-    background: { paper: '#fff', default: '#fafafa' },
-    primary: {
-      light: 'rgba(138, 138, 138, 1)',
-      main: 'rgba(43, 43, 43, 1)',
-      dark: 'rgba(74, 74, 74, 1)',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: 'rgba(229, 229, 229, 1)',
-      main: 'rgba(106, 106, 106, 1)',
-      dark: 'rgba(111, 111, 111, 1)',
-      contrastText: 'rgba(220, 220, 220, 1)'
-    },
-    error: {
-      light: 'rgba(124, 115, 229, 1)',
-      main: 'rgba(28, 14, 186, 1)',
-      dark: 'rgba(77, 47, 211, 1)',
-      contrastText: '#fff'
-    },
-    text: {
-      primary: 'rgba(0, 0, 0, 0.87)',
-      secondary: 'rgba(0, 0, 0, 0.54)',
-      disabled: 'rgba(0, 0, 0, 0.38)',
-      hint: 'rgba(0, 0, 0, 0.38)'
-    },
-    typography: {
-      useNextVariants: true
-    }
+const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  console.log(decodedToken);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = '/login';
+    authenticated = false;
+  } else {
+    authenticated = true;
   }
-});
+}
 
 class App extends Component {
   render() {
@@ -56,8 +42,18 @@ class App extends Component {
             <div className='container'>
               <Switch>
                 <Route exact path='/' component={home} />
-                <Route exact path='/login' component={login} />
-                <Route exact path='/signup' component={signup} />
+                <AuthRoute
+                  exact
+                  path='/login'
+                  component={login}
+                  authenticated={authenticated}
+                />
+                <AuthRoute
+                  exact
+                  path='/signup'
+                  component={signup}
+                  authenticated={authenticated}
+                />
               </Switch>
             </div>
           </Router>
---
+++ frontend/src/util/theme.js
@@ -0,0 +1,33 @@
+export default {
+  palette: {
+    common: { black: '#000', white: '#fff' },
+    background: { paper: '#fff', default: '#fafafa' },
+    primary: {
+      light: 'rgba(138, 138, 138, 1)',
+      main: 'rgba(43, 43, 43, 1)',
+      dark: 'rgba(74, 74, 74, 1)',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: 'rgba(229, 229, 229, 1)',
+      main: 'rgba(106, 106, 106, 1)',
+      dark: 'rgba(111, 111, 111, 1)',
+      contrastText: 'rgba(220, 220, 220, 1)'
+    },
+    error: {
+      light: 'rgba(124, 115, 229, 1)',
+      main: 'rgba(28, 14, 186, 1)',
+      dark: 'rgba(77, 47, 211, 1)',
+      contrastText: '#fff'
+    },
+    text: {
+      primary: 'rgba(0, 0, 0, 0.87)',
+      secondary: 'rgba(0, 0, 0, 0.54)',
+      disabled: 'rgba(0, 0, 0, 0.38)',
+      hint: 'rgba(0, 0, 0, 0.38)'
+    },
+    typography: {
+      useNextVariants: true
+    }
+  }
+};
