a35cde2f669cd0d5057f42f70f0940e4f9fd2535
Josh <joshdibble93@gmail.com>
2019-09-14 15:04:45 +0100

    like and unlike

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -4,14 +4,13 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import EditDetails from './EditDetails';
+import MyButton from '../util/MyButton';
 
 // MUI stuff
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import MuiLink from '@material-ui/core/link';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
 
 // Icons
 import LocationOn from '@material-ui/icons/LocationOn';
@@ -109,11 +108,13 @@ class Profile extends Component {
                 hidden='hidden'
                 onChange={this.handleImageChange}
               />
-              <Tooltip title='Edit profile picture' placement='top'>
-                <IconButton onClick={this.handleEditPicture} className='button'>
-                  <AddAPhotoIcon color='primary' />
-                </IconButton>
-              </Tooltip>
+              <MyButton
+                tip='Edit profile picture'
+                onClick={this.handleEditPicture}
+                btnClassName='button'
+              >
+                <AddAPhotoIcon color='secondary' />
+              </MyButton>
             </div>
             <hr />
             <div className='profile-details'>
@@ -148,11 +149,9 @@ class Profile extends Component {
               {'  '}
               <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
             </div>
-            <Tooltip title='Logout' placement='top'>
-              <IconButton onClick={this.handleLogout}>
-                <KeyboardReturn color='primary' />
-              </IconButton>
-            </Tooltip>
+            <MyButton tip='Log out' onClick={this.handleLogout}>
+              <KeyboardReturn color='secondary' />
+            </MyButton>
             <EditDetails />
           </div>
         </Paper>
