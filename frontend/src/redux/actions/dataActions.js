28d4891769a2f3ee9dead91ad36f5f50dbe92a99
Josh <joshdibble93@gmail.com>
2019-09-18 18:55:49 +0100

    added like and unlike state logic

--- frontend/src/redux/actions/dataActions.js
+++ frontend/src/redux/actions/dataActions.js
@@ -39,4 +39,14 @@ export const likeScream = screamId => dispatch => {
 };
 
 // Unlike a scream
-// copy like here
+export const unlikeScream = screamId => dispatch => {
+  axios
+    .get(`/scream/${screamId}/unlike`)
+    .then(res => {
+      dispatch({
+        type: UNLIKE_SCREAM,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
+};
